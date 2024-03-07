bd163be6b8d6d4334241a7ac411fc93d58100514
Hunter Knott <hunterknott00@gmail.com>
2024-03-06 21:31:20 -0700

    Database list additions
    Every time a new database ID is read from an instruction file, a new database is create and added to a list

--- commandPattern/src/controller/Controller.java
+++ commandPattern/src/controller/Controller.java
@@ -3,22 +3,56 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Controller {
+	private static ArrayList<Database> databases;
+	private ArrayList<Command> commands;
+	
 	private static void readFile(InputStream name) {
-		ArrayList<Command> commands = new ArrayList<>();
 		StringBuilder stringBuilder = new StringBuilder();
 		try (BufferedReader reader = new BufferedReader (new InputStreamReader(name))) {
 			String line;
 			while ((line = reader.readLine()) != null ) {
+				// Add some method here that processes a line
+				processLine(line);
 				stringBuilder.append(line).append("\n");
 			}
 		} catch (IOException e) {
 			System.err.println("Error reading the file: " + e.getMessage());
 		}
-		System.out.println(stringBuilder.toString());
+//		System.out.println(stringBuilder.toString());
 		
 	}
 	
+	private static void processLine(String line) {
+		switch(line.charAt(0)) {
+			case 'A':
+				String[] parts = line.split(" ");
+				String id = parts[1];
+				
+				boolean dbPresent = false;
+				for (Database db : databases) {
+					if (db.getID().equals(id)) {
+						dbPresent = true;
+						break;
+					}
+				}
+				
+				// If there's a new database ID, add that to the databases list
+				if (!dbPresent) {
+					databases.add(makeDatabase(id));
+					System.out.println(databases.size());
+				}
+		}
+		
+		// Add new commands to the command list
+	}
+	
+	private static Database makeDatabase(String id) {
+		return new Database(id);
+	}
+	
 	public void run(String fileName) {
+		databases = new ArrayList<>();
+		
 		// Read the commands, the file name comes from the command line
 		InputStream inputStream = Controller.class.getResourceAsStream(fileName);
 		readFile(inputStream);
