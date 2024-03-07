3ab6743082380270b576334fee58c0899b75a54f
Hunter Knott <hunterknott00@gmail.com>
2024-03-06 20:39:55 -0700

    Reading capability
    Program can read an instruction file

--- commandPattern/src/controller/Controller.java
+++ commandPattern/src/controller/Controller.java
@@ -1,8 +1,28 @@
 package controller;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Controller {
-	public void run() {
+	private static void readFile(InputStream name) {
+		ArrayList<Command> commands = new ArrayList<>();
+		StringBuilder stringBuilder = new StringBuilder();
+		try (BufferedReader reader = new BufferedReader (new InputStreamReader(name))) {
+			String line;
+			while ((line = reader.readLine()) != null ) {
+				stringBuilder.append(line).append("\n");
+			}
+		} catch (IOException e) {
+			System.err.println("Error reading the file: " + e.getMessage());
+		}
+		System.out.println(stringBuilder.toString());
+		
+	}
+	
+	public void run(String fileName) {
 		// Read the commands, the file name comes from the command line
+		InputStream inputStream = Controller.class.getResourceAsStream(fileName);
+		readFile(inputStream);
+		
 		// Make a list of databases from commands
 		// Make a sequence of command objects from commands
 		// Call all execute functions after sequence is complete
--- commandPattern/src/controller/Runner.java
+++ commandPattern/src/controller/Runner.java
@@ -1,8 +1,15 @@
+// Hunter Knott, CS 3450, Utah Valley University
+
 package controller;
 
 public class Runner {
 	public static void main(String [] args) {
+		
+		String fileName = "instructions.dat";
+		if (args.length > 0) {
+			fileName = args[0];
+		}
 		Controller app = new Controller();
-		app.run();
+		app.run(fileName);
 	}
 }
